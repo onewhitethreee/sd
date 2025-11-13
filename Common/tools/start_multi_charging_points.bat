e079e713162419b93da8b957d3e4adce98bd4e9b
onewhitethreee <38567612+onewhitethreee@users.noreply.github.com>
2025-11-13 01:13:33 +0100

    feat(start_scripts): Use variables for host IP in multi charging and driver scripts

--- Common/tools/start_multi_charging_points.bat
+++ Common/tools/start_multi_charging_points.bat
@@ -4,12 +4,14 @@ setlocal enabledelayedexpansion
 set /a loop_count=10
 set /a start_port=5003
 set start_id=cp_001
+set central_host_ip=localhost
+set monitor_ip=localhost
 
 for /l %%i in (1,1,%loop_count%) do (
     set /a current_port=!start_port! + %%i - 1
     set current_id=!start_id:~0,-1!%%i
-    
-    start "EV_CP_E_%%i" cmd /k "python Charging_point\Engine\EV_CP_E.py localhost:9092 --debug_port !current_port!"
+
+    start "EV_CP_E_%%i" cmd /k "python Charging_point\Engine\EV_CP_E.py !central_host_ip!:9092 --debug_port !current_port!"
     timeout /t 1 >nul
-    start "EC_CP_M_%%i" cmd /k "python Charging_point\Monitor\EC_CP_M.py localhost:!current_port! localhost:5002 !current_id!"
+    start "EC_CP_M_%%i" cmd /k "python Charging_point\Monitor\EC_CP_M.py !monitor_ip!:!current_port! !central_host_ip!:5002 !current_id!"
 )
